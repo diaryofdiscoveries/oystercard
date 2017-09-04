131965470d214eb9c6b0c0f100101bc80110198e
Zoe Hopkins <zoe.hopkins@gmail.com>
2017-09-04 16:07:08 +0100

    Update Oystercard with #top_up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,4 +5,13 @@ describe Oystercard do
   it 'has a balance of zero' do
     expect(oystercard.balance).to eq(0)
   end
+
+  describe '#top_up' do
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+
+    it 'tops up by amount given' do
+      expect{ oystercard.top_up 1 }.to change{ oystercard.balance }.by 1
+    end
+  end
+
 end
